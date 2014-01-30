a974722010d62841e60fd9efdf9264a7a6e53d14
Gabriel <belmonte.gabriel.k@gmail.com>
2014-01-30 17:41:29 -0500

    Initial commit

---
+++ src/AutoID3Tagger/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AutoID3Tagger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mArtistName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mSongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mAlbum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mFeatList = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mMagic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder Select:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mArtistName,
+            this.mSongName,
+            this.mAlbum,
+            this.mFeatList});
+            this.dataGridView1.Location = new System.Drawing.Point(41, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(518, 141);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // mArtistName
+            // 
+            this.mArtistName.HeaderText = "Artist Name";
+            this.mArtistName.Name = "mArtistName";
+            // 
+            // mSongName
+            // 
+            this.mSongName.HeaderText = "Song Name";
+            this.mSongName.Name = "mSongName";
+            // 
+            // mAlbum
+            // 
+            this.mAlbum.HeaderText = "Album Name";
+            this.mAlbum.Name = "mAlbum";
+            // 
+            // mFeatList
+            // 
+            this.mFeatList.HeaderText = "Featuring Artist";
+            this.mFeatList.Name = "mFeatList";
+            // 
+            // mMagic
+            // 
+            this.mMagic.Location = new System.Drawing.Point(462, 36);
+            this.mMagic.Name = "mMagic";
+            this.mMagic.Size = new System.Drawing.Size(97, 22);
+            this.mMagic.TabIndex = 4;
+            this.mMagic.Text = "Execute";
+            this.mMagic.UseVisualStyleBackColor = true;
+            this.mMagic.Click += new System.EventHandler(this.mMagic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 343);
+            this.Controls.Add(this.mMagic);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mArtistName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mSongName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAlbum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mFeatList;
+        private System.Windows.Forms.Button mMagic;
+    }
+}
+
---
+++ src/AutoID3Tagger/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoID3Tagger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = new FolderBrowserDialog();
+            DialogResult y = x.ShowDialog();
+            if (y == DialogResult.OK)
+            {
+                this.textBox1.Text = x.SelectedPath;
+            }
+        }
+
+        private void mMagic_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> list =  (List<string>) Directory.EnumerateFiles(this.textBox1.Text);
+                foreach (string item in list)
+                {
+                    string[] stuff = item.Split(new char[] { '-'});
+                    string artist;
+                    string album;
+                    string song;
+                    //string supporting;
+                    if (stuff.Length == 3)
+                    {
+                        artist = stuff[0];
+                        album = stuff[1];
+                        song = stuff[2];
+                    }
+                    else if (stuff.Length == 2)
+                    {
+                        artist = stuff[0];
+                        album = stuff[0];
+                        song = stuff[1];
+                    }
+                    else if (stuff.Length == 1)
+                    {
+                        artist = stuff[0];
+                        album = stuff[0];
+                        song = stuff[0];
+                    }
+
+                    var row = new DataGridViewRow();
+                    this.dataGridView1.Rows.Add(new DataGridViewRow());
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Directory not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException err)
+            {
+                MessageBox.Show("Argument shit happened", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(IOException err)
+            {
+                MessageBox.Show("Some other IO exception", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
