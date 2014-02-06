0edb32103a065f50fb68817a2d77d3346d0f58a0
Gabriel <belmonte.gabriel.k@gmail.com>
2014-02-05 21:12:09 -0500

    Finished the ID3 tagger with 1 bug regarding if the artist is not initially there, it wont be modified. I would have to look into the TagLib Sharp library

--- src/AutoID3Tagger/Form1.Designer.cs
+++ src/AutoID3Tagger/Form1.Designer.cs
@@ -31,13 +31,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableInferred = new System.Windows.Forms.DataGridView();
             this.mArtistName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.mSongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mAlbum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mSongName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mFeatList = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mMagic = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.id3TagInfoTable = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mSave = new System.Windows.Forms.Button();
+            this.mInferSelected = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tableInferred)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.id3TagInfoTable)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -66,33 +76,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dataGridView1
+            // tableInferred
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tableInferred.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableInferred.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.mArtistName,
-            this.mSongName,
             this.mAlbum,
+            this.mSongName,
             this.mFeatList});
-            this.dataGridView1.Location = new System.Drawing.Point(41, 83);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(518, 141);
-            this.dataGridView1.TabIndex = 3;
+            this.tableInferred.Location = new System.Drawing.Point(12, 83);
+            this.tableInferred.Name = "tableInferred";
+            this.tableInferred.Size = new System.Drawing.Size(452, 248);
+            this.tableInferred.TabIndex = 3;
             // 
             // mArtistName
             // 
             this.mArtistName.HeaderText = "Artist Name";
             this.mArtistName.Name = "mArtistName";
             // 
-            // mSongName
-            // 
-            this.mSongName.HeaderText = "Song Name";
-            this.mSongName.Name = "mSongName";
-            // 
             // mAlbum
             // 
             this.mAlbum.HeaderText = "Album Name";
             this.mAlbum.Name = "mAlbum";
+            // 
+            // mSongName
+            // 
+            this.mSongName.HeaderText = "Song Name";
+            this.mSongName.Name = "mSongName";
             // 
             // mFeatList
             // 
@@ -103,25 +113,102 @@
             // 
             this.mMagic.Location = new System.Drawing.Point(462, 36);
             this.mMagic.Name = "mMagic";
-            this.mMagic.Size = new System.Drawing.Size(97, 22);
+            this.mMagic.Size = new System.Drawing.Size(100, 25);
             this.mMagic.TabIndex = 4;
-            this.mMagic.Text = "Execute";
+            this.mMagic.Text = "Read";
             this.mMagic.UseVisualStyleBackColor = true;
             this.mMagic.Click += new System.EventHandler(this.mMagic_Click);
+            // 
+            // id3TagInfoTable
+            // 
+            this.id3TagInfoTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.id3TagInfoTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn4});
+            this.id3TagInfoTable.Location = new System.Drawing.Point(481, 83);
+            this.id3TagInfoTable.Name = "id3TagInfoTable";
+            this.id3TagInfoTable.Size = new System.Drawing.Size(449, 248);
+            this.id3TagInfoTable.TabIndex = 5;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Artist Name";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Album Name";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Song Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Featuring Artist";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Inferred information from file name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(489, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Information from ID3 tags";
+            // 
+            // mSave
+            // 
+            this.mSave.Location = new System.Drawing.Point(809, 35);
+            this.mSave.Name = "mSave";
+            this.mSave.Size = new System.Drawing.Size(100, 25);
+            this.mSave.TabIndex = 8;
+            this.mSave.Text = "Save Tags";
+            this.mSave.UseVisualStyleBackColor = true;
+            this.mSave.Click += new System.EventHandler(this.mSave_Click);
+            // 
+            // mInferSelected
+            // 
+            this.mInferSelected.Location = new System.Drawing.Point(703, 36);
+            this.mInferSelected.Name = "mInferSelected";
+            this.mInferSelected.Size = new System.Drawing.Size(100, 25);
+            this.mInferSelected.TabIndex = 9;
+            this.mInferSelected.Text = "Infer Selected";
+            this.mInferSelected.UseVisualStyleBackColor = true;
+            this.mInferSelected.Click += new System.EventHandler(this.mInferSelected_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 343);
+            this.ClientSize = new System.Drawing.Size(953, 343);
+            this.Controls.Add(this.mInferSelected);
+            this.Controls.Add(this.mSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.id3TagInfoTable);
             this.Controls.Add(this.mMagic);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableInferred);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "AutoID3Tagger";
+            ((System.ComponentModel.ISupportInitialize)(this.tableInferred)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.id3TagInfoTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,12 +219,21 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn mArtistName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn mSongName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn mAlbum;
-        private System.Windows.Forms.DataGridViewTextBoxColumn mFeatList;
+        private System.Windows.Forms.DataGridView tableInferred;
         private System.Windows.Forms.Button mMagic;
+        private System.Windows.Forms.DataGridView id3TagInfoTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button mSave;
+        private System.Windows.Forms.Button mInferSelected;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mArtistName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAlbum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mSongName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mFeatList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
     }
 }
 
--- src/AutoID3Tagger/Form1.cs
+++ src/AutoID3Tagger/Form1.cs
@@ -13,9 +13,20 @@ namespace AutoID3Tagger
 {
     public partial class Form1 : Form
     {
+        int mOldHeight;
         public Form1()
         {
             InitializeComponent();
+            this.SizeChanged += Form1_SizeChanged;
+            mOldHeight = this.Height;
+        }
+
+        void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            var delta = Height - mOldHeight;
+            id3TagInfoTable.Height += delta;
+            tableInferred.Height += delta;
+            mOldHeight = Height;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,37 +41,78 @@ namespace AutoID3Tagger
 
         private void mMagic_Click(object sender, EventArgs e)
         {
+            tableInferred.Rows.Clear();
+            id3TagInfoTable.Rows.Clear();
             try
             {
-                List<string> list =  (List<string>) Directory.EnumerateFiles(this.textBox1.Text);
+                IEnumerable<string> list =  Directory.EnumerateFiles(this.textBox1.Text);
+                
                 foreach (string item in list)
                 {
-                    string[] stuff = item.Split(new char[] { '-'});
+                    string fileName = Path.GetFileNameWithoutExtension(item);
+                    string[] stuff = fileName.Split(new char[] { '-' });
                     string artist;
                     string album;
                     string song;
+                    string supArts = string.Empty;
+                    
                     //string supporting;
                     if (stuff.Length == 3)
                     {
-                        artist = stuff[0];
-                        album = stuff[1];
-                        song = stuff[2];
+                        artist = stuff[0].Trim();
+                        album = stuff[1].Trim();
+                        song = stuff[2].Trim();
                     }
                     else if (stuff.Length == 2)
                     {
-                        artist = stuff[0];
-                        album = stuff[0];
-                        song = stuff[1];
+                        artist = stuff[0].Trim();
+                        album = stuff[0].Trim();
+                        song = stuff[1].Trim();
                     }
                     else if (stuff.Length == 1)
                     {
-                        artist = stuff[0];
-                        album = stuff[0];
-                        song = stuff[0];
+                        artist = stuff[0].Trim();
+                        album = stuff[0].Trim();
+                        song = stuff[0].Trim();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect format for " + item, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        continue;
+                    }
+                    var featSplit =  song.Split(new string[]  { "ft."} , StringSplitOptions.None );;
+                    song = featSplit[0].Trim();
+                    if (featSplit.Length > 1)
+                    {
+                        supArts = featSplit[1].Trim();
                     }
 
                     var row = new DataGridViewRow();
-                    this.dataGridView1.Rows.Add(new DataGridViewRow());
+                    row.Tag = item;
+                    row.CreateCells(tableInferred, artist, album, song, supArts);
+                    this.tableInferred.Rows.Add(row);
+
+                    /*Read info from tags*/
+                    string id3Artist;
+                    string id3Album;
+                    string id3SongName;
+                    string id3SupArtist;
+                    
+                    TagLib.File info = TagLib.File.Create(item);
+                    id3Artist = info.Tag.FirstAlbumArtist != null? info.Tag.FirstAlbumArtist.Trim(): "";
+                    id3Album = info.Tag.Album != null? info.Tag.Album.Trim() : "";
+                    id3SongName = info.Tag.Title != null ? info.Tag.Title.Trim() : "";
+                    var allArts = info.Tag.AlbumArtists != null ? info.Tag.AlbumArtists.ToList() : new List<string>(0);
+                    if(allArts.Count >= 1)
+                        allArts.RemoveAt(0);
+                    id3SupArtist = string.Join(";" , allArts.ToArray());
+
+                    var nowRow = new DataGridViewRow();
+                    nowRow.Tag = item;
+                    nowRow.CreateCells(this.id3TagInfoTable, id3Artist, id3Album, id3SongName, id3SupArtist);
+                    this.id3TagInfoTable.Rows.Add(nowRow);
+                    info.Dispose();
+
                 }
             }
             catch (DirectoryNotFoundException)
@@ -69,13 +121,75 @@ namespace AutoID3Tagger
             }
             catch (ArgumentException err)
             {
-                MessageBox.Show("Argument shit happened", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Argument Exception Happened: \n"+ err.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch(IOException err)
             {
-                MessageBox.Show("Some other IO exception", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Some other IO exception: \n" + err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void mInferSelected_Click(object sender, EventArgs e)
+        {
+            var rows = tableInferred.SelectedRows;
+            if (rows.Count == 0)
+            {
+                MessageBox.Show("No rows selected. Make sure to select the row, not the cell.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            foreach (DataGridViewRow row in rows)
+            {
+                var otherRows = id3TagInfoTable.Rows;
+                DataGridViewRow match = null;
+                //find the corresponding row
+                foreach (DataGridViewRow oRow in otherRows)
+                {
+                    if (oRow.Tag == row.Tag)
+                    {
+                        match = oRow;
+                        break;
+                    }
+                }
+
+                for (var ii = 0; ii < 4; ii++ )
+                {
+                    match.Cells[ii].Value = row.Cells[ii].Value;
+                }
+            }
+        }
+
+        private void mSave_Click(object sender, EventArgs e)
+        {
+            var rows = id3TagInfoTable.SelectedRows;
+            if (rows.Count == 0)
+            {
+                MessageBox.Show("No rows selected. Make sure to select the row, not the cell.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (DataGridViewRow row in rows)
+            {
+                TagLib.File file = TagLib.File.Create((string)row.Tag);
+                string[] artistArr = ((string)row.Cells[3].Value).Split(new char[] {';'},StringSplitOptions.RemoveEmptyEntries);
+                List<string> artistList = new List<string>(artistArr.Length + 1);
+                artistList.Add((string)row.Cells[1].Value);
+                artistList.AddRange( artistArr  );
+
+                
+                file.Tag.Album = (string)row.Cells[1].Value;
+                file.Tag.Title = (string)row.Cells[2].Value;
+                
+                file.Tag.AlbumArtists = null;
+                file.Tag.AlbumArtists = artistList.ToArray();
+                //file.Tag.Performers = artistList.ToArray();
+                file.Save();
+                file.Dispose();
+            }
+
+            MessageBox.Show("Selected Rows saved", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
 
 
     }
